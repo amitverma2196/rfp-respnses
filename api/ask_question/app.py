5b58bfe0045e44097f04cb9b7953ae50e6028538
amitverma2196 <amitverma2196@gmail.com>
2025-02-13 12:09:03 +0530

    Add files via upload

--- api/ask_question/app.py
+++ api/ask_question/app.py
@@ -1,1 +1,28 @@
+import boto3
+import json
+import os
+from shared.utils import get_rag_response
 
+bedrock_client = boto3.client('bedrock')
+
+def lambda_handler(event, context):
+    try:
+        # Parse the API Gateway event body
+        body = json.loads(event['body'])
+        knowledgebase_id = os.environ['KNOWLEDGEBASE_ID'] 
+        question = body['question']
+        
+        # Get the RAG response
+        response = get_rag_response(bedrock_client, knowledgebase_id, question)
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'response': response
+            })
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
